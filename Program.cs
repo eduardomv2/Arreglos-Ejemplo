467b958dbac0b78e72412c7a8b571d1788fffc44
eduar <eduar@DESKTOP-SV6GQVL>
2023-03-14 22:30:40 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arreglos_Ejemplo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crear una matriz de 3x3
+            int[,] matriz = new int[3, 3];
+
+            // Inicializar la matriz con algunos valores
+            matriz[0, 0] = 1;
+            matriz[0, 1] = 2;
+            matriz[0, 2] = 3;
+            matriz[1, 0] = 4;
+            matriz[1, 1] = 5;
+            matriz[1, 2] = 6;
+            matriz[2, 0] = 7;
+            matriz[2, 1] = 8;
+            matriz[2, 2] = 9;
+
+            // Imprimir la matriz en la consola
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    Console.Write(matriz[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
